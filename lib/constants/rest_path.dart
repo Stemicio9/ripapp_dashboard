f6575dd7971c00bfc230e602411713722f476ab4
Pasquale De Nisi <pdenisi04530@gmail.com>
2023-06-30 13:18:50 +0200

    modifiche per deploy

--- lib/constants/rest_path.dart
+++ lib/constants/rest_path.dart
@@ -6,10 +6,10 @@ const double appVersion = 0.1;
 //const String baseUrl = "http://79.61.39.115:9999";
 
 
-const String baseUrl = "http://ripapp.it"; //IP CURRENTLY USED IN PRODUCTION
+//const String baseUrl = "http://ripapp.it"; //IP CURRENTLY USED IN PRODUCTION
 //const String baseUrl = "http://192.168.1.99:48795";
 //const String baseUrl = "http://localhost:8081";
 // const String baseUrl = "http://172.17.0.3:80";
 
 //ip backend locale
-//const String baseUrl = "http://localhost:8080";
+const String baseUrl = "http://localhost:8080";
