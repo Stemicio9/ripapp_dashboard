2ff0aba79fca5429562d82a0f25ea5d8f2467f80
twentyfive <simonebria20@gmail.com>
2023-07-26 13:20:43 +0200

    refactor demise_detail.dart

--- lib/pages/internal_pages/agency_pages/demise_detail.dart
+++ lib/pages/internal_pages/agency_pages/demise_detail.dart
@@ -4,12 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ripapp_dashboard/blocs/CurrentPageCubit.dart';
 import 'package:ripapp_dashboard/blocs/profile_image_cubit.dart';
 import 'package:ripapp_dashboard/blocs/selected_demise_cubit.dart';
-import 'package:ripapp_dashboard/constants/images_constants.dart';
 import 'package:ripapp_dashboard/constants/kinships.dart';
 import 'package:ripapp_dashboard/constants/route_constants.dart';
 import 'package:ripapp_dashboard/models/DemiseRelative.dart';
 import 'package:ripapp_dashboard/models/city_from_API.dart';
-import 'package:ripapp_dashboard/models/demise_entity.dart';
 import 'package:ripapp_dashboard/pages/internal_pages/agency_pages/widgets/add_relative.dart';
 import 'package:ripapp_dashboard/pages/internal_pages/agency_pages/widgets/deceased_detail.dart';
 import 'package:ripapp_dashboard/pages/internal_pages/agency_pages/widgets/funeral_detail.dart';
@@ -31,30 +29,10 @@ class DemiseDetailState extends State<DemiseDetail> {
   CurrentPageCubit get _currentPageCubit => context.read<CurrentPageCubit>();
   ProfileImageCubit get _profileImageCubit => context.read<ProfileImageCubit>();
   SelectedDemiseCubit  get _selectedDemiseCubit => context.read<SelectedDemiseCubit>();
-  var imageFile = ImagesConstants.imgDemisePlaceholder;
-  var obituaryName = "";
-  var obituaryUrl = "";
-  String firstName = "";
-  String lastName = "";
-  String age = "";
-  String id = "";
-  String deceasedDate = "";
-  String city = "";
-  String phoneNumber = "";
-  String funeralDate = "";
-  String funeralNote = "";
-  String funeralAddress = "";
-  String funeralHour = "";
-  String wakeHour = "";
-  String wakeNote = "";
-  String wakeAddress = "";
-  String wakeDate = "";
   String missingData = "Dato non inserito";
   final TextEditingController relativeController = TextEditingController();
   late List<DemiseRelative> relativeList = [];
-  late List<CityFromAPI> citiesOfInterest = [];
   List<Widget> relativeRows = [];
-
 
   @override
   void initState() {
@@ -75,10 +53,9 @@ class DemiseDetailState extends State<DemiseDetail> {
     _currentPageCubit.changeCurrentPage(RouteConstants.demiseDetail);
     return BlocBuilder<ProfileImageCubit, ProfileImageState>(
         builder: (context, imageState) {
-      obituaryName = extractFileNameFromFirebaseUrl(imageState.obituaryUrl);
           return BlocBuilder<SelectedDemiseCubit, SelectedDemiseState>(
               builder: (context, state) {
-                fillValues(state.selectedDemise);
+                relativeList = state.selectedDemise.relatives ?? [];
                 createRelative();
                 return ScaffoldWidget(
                   body: SingleChildScrollView(
@@ -96,25 +73,25 @@ class DemiseDetailState extends State<DemiseDetail> {
                           imageState.loaded ? DeceasedDetail(
                             imageFile: imageState.imageUrl,
                             downloadObituary: (){downloadFile(imageState.obituaryUrl);},
-                            obituaryName: obituaryName,
-                            id: id,
-                            age: age,
-                            lastName: lastName,
-                            firstName: firstName,
-                            phoneNumber: phoneNumber,
-                            city: city,
-                            citiesOfInterest: citiesOfInterest,
-                            deceasedDate: deceasedDate,
+                            obituaryName: extractFileNameFromFirebaseUrl(imageState.obituaryUrl),
+                            id:state.selectedDemise.id != null ? state.selectedDemise.id.toString() : missingData,
+                            age: state.selectedDemise.age != null ? state.selectedDemise.age.toString() : missingData,
+                            lastName: state.selectedDemise.lastName ?? missingData,
+                            firstName: state.selectedDemise.firstName ?? missingData,
+                            phoneNumber: state.selectedDemise.phoneNumber ?? missingData,
+                            city: state.selectedDemise.city != null ? state.selectedDemise.city.toString() : missingData,
+                            citiesOfInterest: state.selectedDemise.cities != null ? state.selectedDemise.cities!.map((e) => CityFromAPI(name: e.name)).toList() : [],
+                            deceasedDate: state.selectedDemise.deceasedDate != null ? StringFormatters().getFormattedDate(state.selectedDemise.deceasedDate) : missingData,
                           ) : Container(),
 
                           //wake data
                           Padding(
                             padding: const EdgeInsets.only(top: 20),
                             child: WakeDetail(
-                                wakeDate: wakeDate,
-                                wakeNote: wakeNote,
-                                wakeHour: wakeHour,
-                                wakeAddress: wakeAddress,
+                                wakeDate:  StringFormatters().getFormattedDate(state.selectedDemise.wakeDateTime),
+                                wakeNote:  state.selectedDemise.wakeNotes ?? missingData,
+                                wakeHour:  StringFormatters().getFormattedTime(state.selectedDemise.wakeDateTime),
+                                wakeAddress:  state.selectedDemise.wakeAddress ?? missingData,
 
                             ),
                           ),
@@ -123,10 +100,10 @@ class DemiseDetailState extends State<DemiseDetail> {
                           Padding(
                               padding: const EdgeInsets.only(top: 20),
                               child: FuneralDetail(
-                                  funeralDate: funeralDate,
-                                  funeralNote: funeralNote,
-                                  funeralHour: funeralHour,
-                                  funeralAddress: funeralAddress,
+                                  funeralDate: StringFormatters().getFormattedDate(state.selectedDemise.funeralDateTime),
+                                  funeralNote: state.selectedDemise.funeralNotes ?? missingData,
+                                  funeralHour: StringFormatters().getFormattedTime(state.selectedDemise.funeralDateTime),
+                                  funeralAddress: state.selectedDemise.funeralAddress ?? missingData,
                               )
                           ),
 
@@ -165,37 +142,4 @@ class DemiseDetailState extends State<DemiseDetail> {
     }
     return '';
   }
-
-  void fillValues(DemiseEntity demiseEntity) {
-    firstName = demiseEntity.firstName != null ? demiseEntity.firstName.toString() : missingData;
-    lastName =  demiseEntity.lastName != null ? demiseEntity.lastName.toString() : missingData;
-    phoneNumber = demiseEntity.phoneNumber != null ? demiseEntity.phoneNumber.toString() : missingData;
-    age =  demiseEntity.age != null ? demiseEntity.age.toString() : missingData;
-    id = demiseEntity.id.toString();
-    citiesOfInterest = demiseEntity.cities != null ? demiseEntity.cities!.map((e) => CityFromAPI(name: e.name)).toList(): [];
-    city = demiseEntity.city != null  ? demiseEntity.city.toString() : missingData;
-    phoneNumber = demiseEntity.phoneNumber != null ? demiseEntity.phoneNumber.toString() : missingData;
-    deceasedDate = demiseEntity.deceasedDate != null ? StringFormatters().getFormattedDate(demiseEntity.deceasedDate) : missingData;
-    funeralNote = demiseEntity.funeralNotes != null ? demiseEntity.funeralNotes.toString() : missingData;
-    funeralAddress = demiseEntity.funeralAddress != null ? demiseEntity.funeralAddress.toString() : missingData;
-    if (demiseEntity.funeralDateTime != null){
-      funeralDate = StringFormatters().getFormattedDate(demiseEntity.funeralDateTime);
-      funeralHour = StringFormatters().getFormattedTime(demiseEntity.funeralDateTime);
-    } else {
-      wakeDate = missingData;
-      wakeHour = missingData;
-    }
-    wakeNote = demiseEntity.wakeNotes != null ? demiseEntity.wakeNotes.toString() : missingData;
-    wakeAddress = demiseEntity.wakeAddress != null ? demiseEntity.wakeAddress.toString() : missingData;
-    if (demiseEntity.wakeDateTime != null){
-      wakeDate = StringFormatters().getFormattedDate(demiseEntity.wakeDateTime);
-      wakeHour = StringFormatters().getFormattedTime(demiseEntity.wakeDateTime);
-    } else {
-      wakeDate = missingData;
-      wakeHour = missingData;
-    }
-
-    relativeList = demiseEntity.relatives ?? [];
-  }
-
 }
